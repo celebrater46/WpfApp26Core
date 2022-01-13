8bc26f7a62004f8e4f27fe9a1e34ada563f352fc
celebrater <tkgm45nbchk@gmail.com>
2022-01-13 16:23:10 +0900

    wrote all code

--- WpfApp26Core/MainWindow.xaml.cs
+++ WpfApp26Core/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.VisualBasic.FileIO;
+using DIMG = System.Drawing.Imaging;
+using DRW = System.Drawing;
+using MIMG = System.Windows.Media.Imaging;
+using Path = System.IO.Path;
 
 namespace WpfApp26Core
 {
@@ -20,9 +26,100 @@ namespace WpfApp26Core
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly int captureWaitMSec = 300;
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr objectHandle);
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void CaptureButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            captureImage.Source = null;
+            WindowState = WindowState.Minimized;
+            await Task.Delay(captureWaitMSec);
+            captureImage.Source = GetFullScreenImage();
+            WindowState = WindowState.Normal;
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (captureImage.Source == null)
+            {
+                MessageBox.Show(
+                    "It needs to capture first.",
+                    "Warning",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+                return;
+            }
+
+            SaveImage();
+            await NotifySaveImage();
+        }
+
+        private async void CaptureAndSaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+            await Task.Delay(captureWaitMSec);
+            captureImage.Source = GetFullScreenImage();
+            SaveImage();
+            WindowState = WindowState.Normal;
+        }
+
+        private MIMG.BitmapSource GetFullScreenImage()
+        {
+            int w = (int)SystemParameters.PrimaryScreenWidth;
+            int h = (int)SystemParameters.PrimaryScreenHeight;
+
+            using (var bmp = new DRW.Bitmap(w, h, DIMG.PixelFormat.Format32bppRgb))
+            using(var grph = DRW.Graphics.FromImage(bmp))
+            {
+                grph.CopyFromScreen(sourceX: 0, sourceY: 0, destinationX: 0, destinationY: 0, bmp.Size);
+                IntPtr bmpHandle = bmp.GetHbitmap();
+
+                try
+                {
+                    return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                        bmpHandle, 
+                        IntPtr.Zero, 
+                        Int32Rect.Empty, 
+                        MIMG.BitmapSizeOptions.FromEmptyOptions());
+                }
+                finally
+                {
+                    DeleteObject(bmpHandle);
+                }
+            }
+        }
+
+        private void SaveImage()
+        {
+            string saveFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                "Screenshots");
+            string saveFilePath = Path.Combine(saveFolder, $"Screenshot{DateTime.Now:yyyyMMdd-HHmmss}.jpeg");
+
+            if (!Directory.Exists(saveFolder))
+            {
+                Directory.CreateDirectory(saveFolder);
+            }
+
+            using (Stream stream = new FileStream(saveFilePath, FileMode.Create))
+            {
+                var encoder = new MIMG.JpegBitmapEncoder();
+                encoder.Frames.Add(MIMG.BitmapFrame.Create((MIMG.BitmapSource)captureImage.Source));
+                encoder.Save(stream);
+            }
+        }
+
+        private async Task NotifySaveImage()
+        {
+            saveInfo.Visibility = Visibility.Visible;
+            await Task.Delay(250);
+            saveInfo.Visibility = Visibility.Collapsed;
         }
     }
 }
